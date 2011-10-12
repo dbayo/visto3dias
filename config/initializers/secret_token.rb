0b214a436f55bc7f6af57903205d6c3644ab97f2
dbayo <david.bayo.nieto@gmail.com>
2011-10-12 13:08:25 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visto3dias::Application.config.secret_token = 'a1149758b08cf90426c75bbdafb899d14714842b31415fe8fd3451611c4d5b44efa18352d7c69d51fb8d466907027a4939514e675f4b65e265885338dafaaa6c'
